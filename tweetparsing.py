6bdcf260964573219a2e71d6cd866e55a9b4b434
Sudakshina Mandal <sudakshina184@gmail.com>
2018-04-25 17:34:08 +0530

    change in parsing

--- tweetparsing.py
+++ tweetparsing.py
@@ -21,23 +21,20 @@ tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
 tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
 tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
 
-##tweets_by_lang = tweets['lang'].value_counts()
+tweets_by_lang = tweets['lang'].value_counts()
 
-##tweets_by_lang = tweets['lang'].value_counts
-##print (tweets['lang'].value_counts()[:5])
 
 tweets['lang']
-##print (tweets['lang'].value_counts()[:5])
 
 
-##fig, ax = plt.subplots()
-##ax.tick_params(axis='x', labelsize=15)
-##ax.tick_params(axis='y', labelsize=10)
-##ax.set_ylabel('Languages', fontsize=10)
-##ax.set_xlabel('Number of tweets' , fontsize=10)
-##ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
-##tweets_by_lang.plot(ax=ax, kind='barh', color='red')
-##plt.show()
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_ylabel('Languages', fontsize=10)
+ax.set_xlabel('Number of tweets' , fontsize=10)
+ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+tweets_by_lang.plot(ax=ax, kind='barh', color='red')
+plt.show()
 
 tweets_by_country = tweets['country'].value_counts()
 
